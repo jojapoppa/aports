3f9be37bd8b81221be46307d77d06c694b58982c
Roman Shaposhnik <rvs@zededa.com>
2021-05-10 11:22:42 -0700

    Adding GRUB

--- scripts/mkimg.riscv.sh
+++ scripts/mkimg.riscv.sh
@@ -1,17 +1,17 @@
-# [abuild-]apk add abuild apk-tools alpine-conf busybox fakeroot xorriso mtools squashfs-tools mkinitfs
+# [abuild-]apk add abuild apk-tools alpine-conf busybox fakeroot xorriso mtools squashfs-tools mkinitfs grub grub-efi u-boot
 # ln -s python3.9 /usr/bin/python3
 # 
 # ./mkimage.sh [--hostkeys] --outdir /tmp/out --profile riscv --repository http://eve-alpine-packages.s3.amazonaws.com/edge/main --extra-repository http://eve-alpine-packages.s3.amazonaws.com/edge/community
 
 profile_riscv() {
 	profile_base
-	title="Generic RISCV64"
+	title="Generic RISCV"
 	desc="Has default RISCV64 kernel.
-		Includes the uboot bootloader.
-		Supports armv7 and aarch64."
+		Includes the u-boot and GRUB bootloaders.
+		Supports only riscv64 for now."
 	image_ext="tar.gz"
 	arch="riscv64"
-	kernel_flavors="virt"
+	kernel_flavors="lts"
 	hostname="alpine"
 	uboot_install="yes"
 }
